8320a9e47a7aa7bfe01389d98b02b7697c56706d
sudo-Eric <ericheinke0@gmail.com>
2022-09-27 16:59:15 -0700

    Improved initial packet code.

--- DecodeStatusCodes.h
+++ DecodeStatusCodes.h
@@ -15,113 +15,198 @@ using namespace std;
 class DecodeStatusCodes
 {
 public:
-    DecodeStatusCodes();
-    DecodeStatusCodes(bool mode);
+    DecodeStatusCodes(const char*, bool mode);
     ~DecodeStatusCodes();
 
-    bool flushReceiver(char code);
-    bool openDevice(char code);
-    bool readBytes(int code);
-    bool readChar(char code);
-    bool readString(int code);
-    bool writeBytes(char code);
-    bool writeChar(char code);
-    bool writeString(char code);
+    bool flushReceiver(const char code);
+    bool openDevice(const char code);
+    bool readBytes(const int code);
+    bool readChar(const char code);
+    bool readString(const int code);
+    bool writeBytes(const char code);
+    bool writeChar(const char code);
+    bool writeString(const char code);
 private:
+    const char *serial_port;
     bool showSuccess;
 };
 
-DecodeStatusCodes::DecodeStatusCodes() {showSuccess = true;}
-DecodeStatusCodes::DecodeStatusCodes(bool showSuccess)
-{
-    this->showSuccess = showSuccess;
-    //mode true: Display success and failure messages
-    //mode false: Display only failure messages
-}
-DecodeStatusCodes::~DecodeStatusCodes() {}
-
-bool DecodeStatusCodes::flushReceiver(char code)
-{
-    if (code != 0) {
-        if (showSuccess) {cout<<"Receiver succesfully flushed"<<endl;}           return true;
-    } else {              cout<<"Error: reciever not flushed succesfully"<<endl; return false;}
+DecodeStatusCodes::DecodeStatusCodes(const char *serial_port, bool showSuccess):
+serial_port(serial_port),
+showSuccess(showSuccess)
+// mode true: Display success and failure messages
+// mode false: Display only failure messages
+{ }
+DecodeStatusCodes::~DecodeStatusCodes() {
+    delete[] this->serial_port;
 }
 
-bool DecodeStatusCodes::openDevice(char code)
+bool DecodeStatusCodes::flushReceiver(const char code)
 {
-    #if defined (_WIN32) || defined(_WIN64)
-        #define SERIAL_PORT "\\\\.\\COM4"
-    #endif
-    #if defined (__linux__) || defined(__APPLE__)
-        #define SERIAL_PORT "/dev/ttyACM0"
-    #endif
-
-    if      (code ==  1) {
-        if (true)        {printf("Successful connection to %s\n",SERIAL_PORT);}                     return true;}
-    else if (code == -1) {printf("Error: device not found %s\n",SERIAL_PORT);                       return false;}
-    else if (code == -2) {printf("Error: error while opening the device %s\n",SERIAL_PORT);         return false;}
-    else if (code == -3) {printf("Error: error while getting port parameters %s\n",SERIAL_PORT);    return false;}
-    else if (code == -4) {printf("Error: Speed (Bauds) not recognized %s\n",SERIAL_PORT);           return false;}
-    else if (code == -5) {printf("Error: error while writing port parameters %s\n",SERIAL_PORT);    return false;}
-    else if (code == -6) {printf("Error: error while writing timeout parameters %s\n",SERIAL_PORT); return false;}
-    else if (code == -7) {printf("Error: Databits not recognized %s\n",SERIAL_PORT);                return false;}
-    else if (code == -8) {printf("Error: Stopbits not recognized %s\n",SERIAL_PORT);                return false;}
-    else if (code == -9) {printf("Error: Parity not recognized %s\n",SERIAL_PORT);                  return false;}
-    else                 {cout<<"Error: an unknown error has occured"<<endl;                        return false;}
+    // If code != 0
+    if (code) {
+        if (showSuccess) {
+            cout<<"Receiver succesfully flushed"<<endl;
+        }
+        return true;
+    } else {
+        cout<<"Error: reciever not flushed succesfully"<<endl;
+        return false;
+    }
 }
 
-bool DecodeStatusCodes::readBytes(int code)
+bool DecodeStatusCodes::openDevice(const char code)
 {
-    if      (code >  -1) {
-        if (showSuccess) {cout<<"Bytes read succesfully"<<endl;}                return true;}
-    else if (code == -1) {cout<<"Error: error while setting the Timeout"<<endl; return false;}
-    else if (code == -2) {cout<<"Error: error while reading the byte"<<endl;    return false;}
-    else                 {cout<<"Error: an unknown error has occured"<<endl;    return false;}
+    switch (code) {
+        case 1:
+            if (true)
+                printf("Successful connection to %s\n",this->serial_port);
+            return true;
+        case -1:
+            printf("Error: device not found %s\n",this->serial_port);
+            return false;
+        case -2:
+            printf("Error: error while opening the device %s\n",this->serial_port);
+            return false;
+        case -3:
+            printf("Error: error while getting port parameters %s\n",this->serial_port);
+            return false;
+        case -4:
+            printf("Error: Speed (Bauds) not recognized %s\n",this->serial_port);
+            return false;
+        case -5:
+            printf("Error: error while writing port parameters %s\n",this->serial_port);
+            return false;
+        case -6:
+            printf("Error: error while writing timeout parameters %s\n",this->serial_port);
+            return false;
+        case -7:
+            printf("Error: Databits not recognized %s\n",this->serial_port);
+            return false;
+        case -8:
+            printf("Error: Stopbits not recognized %s\n",this->serial_port);
+            return false;
+        case -9:
+            printf("Error: Parity not recognized %s\n",this->serial_port);
+            return false;
+        default:
+            printf("Error: an unknown error has occured\n");
+            return false;  
+    };
 }
 
-bool DecodeStatusCodes::readChar(char code)
+bool DecodeStatusCodes::readBytes(const int code)
 {
-    if      (code ==  1) {
-        if (showSuccess) {cout<<"Character read succesfully"<<endl;}            return true;}
-    else if (code ==  0) {cout<<"Error: Timeout reached"<<endl;                 return false;}
-    else if (code == -1) {cout<<"Error: error while setting the Timeout"<<endl; return false;}
-    else if (code == -2) {cout<<"Error: error while reading the byte"<<endl;    return false;}
-    else                 {cout<<"Error: an unknown error has occured"<<endl;    return false;}
+
+    if (code > -1) {
+        if (showSuccess) {
+            cout<<"Bytes read succesfully"<<endl;
+        }
+        return true;
+    } else if (code == -1) {
+        cout<<"Error: error while setting the Timeout"<<endl;
+        return false;
+    } else if (code == -2) {
+        cout<<"Error: error while reading the byte"<<endl;
+        return false;
+    } else {
+        cout<<"Error: an unknown error has occured"<<endl;
+        return false;
+    }
 }
 
-bool DecodeStatusCodes::readString(int code)
+bool DecodeStatusCodes::readChar(const char code)
 {
-    if      (code >   0) {
-        if (showSuccess) {cout<<"String read succesfully"<<endl;}               return true;}
-    else if (code ==  0) {cout<<"Error: timeout reached"<<endl;                 return false;}
-    else if (code == -1) {cout<<"Error: error while setting the Timeout"<<endl; return false;}
-    else if (code == -2) {cout<<"Error: error while reading the byte"<<endl;    return false;}
-    else if (code == -3) {cout<<"Error: MaxNbBytes is reached"<<endl;           return false;}
-    else                 {cout<<"Error: an unknown error has occured"<<endl;    return false;}
+    switch (code) {
+        case 1:
+            if (showSuccess) 
+                cout<<"Character read succesfully"<<endl;
+            return true;
+        case 0:
+            cout<<"Error: Timeout reached"<<endl;
+            return false;
+        case -1:
+            cout<<"Error: error while setting the Timeout"<<endl;
+            return false;
+        case -2:
+            cout<<"Error: error while reading the byte"<<endl;
+            return false;
+        default:
+            cout<<"Error: an unknown error has occured"<<endl;
+            return false;
+    };
 }
 
-bool DecodeStatusCodes::writeBytes(char code)
+bool DecodeStatusCodes::readString(const int code)
 {
-    if      (code ==  1) {
-        if (showSuccess) {cout<<"Bytes written succesfully"<<endl;}             return true;}
-    else if (code == -1) {cout<<"Error: error while writting data"<<endl;       return false;}
-    else                 {cout<<"Error: an unknown error has occured"<<endl;    return false;}
+    if (code >   0) {
+        if (showSuccess) 
+            cout<<"String read succesfully"<<endl;
+        return true;
+    } else if (code ==  0) {
+        cout<<"Error: timeout reached"<<endl;
+        return false;
+    } else if (code == -1) {
+        cout<<"Error: error while setting the Timeout"<<endl;
+        return false;
+    } else if (code == -2) {
+        cout<<"Error: error while reading the byte"<<endl;
+        return false;
+    } else if (code == -3) {
+        cout<<"Error: MaxNbBytes is reached"<<endl;
+        return false;
+    } else {
+        cout<<"Error: an unknown error has occured"<<endl;
+        return false;
+    }
 }
 
-bool DecodeStatusCodes::writeChar(char code)
+bool DecodeStatusCodes::writeBytes(const char code)
 {
-    if      (code ==  1) {
-        if (showSuccess) {cout<<"Character written succesfully"<<endl;}         return true;}
-    else if (code == -1) {cout<<"Error: error while writting data"<<endl;       return false;}
-    else                 {cout<<"Error: an unknown error has occured"<<endl;    return false;}
+    switch (code) {
+        case 1:
+            if (showSuccess)
+                cout<<"Bytes written succesfully"<<endl;
+            return true;
+        case -1:
+            cout<<"Error: error while writting data"<<endl;
+            return false;
+        default:
+            cout<<"Error: an unknown error has occured"<<endl;
+            return false;
+    };
 }
 
-bool DecodeStatusCodes::writeString(char code)
+bool DecodeStatusCodes::writeChar(const char code)
 {
-    if      (code ==  1) {
-        if (showSuccess) {cout<<"String written succesfully"<<endl;}            return true;}
-    else if (code == -1) {cout<<"Error: error while writting data"<<endl;       return false;}
-    else                 {cout<<"Error: an unknown error has occured"<<endl;    return false;}
+    switch (code) {
+        case 1:
+            if (showSuccess)
+                cout<<"Character written succesfully"<<endl;
+            return true;
+        case -1:
+            cout<<"Error: error while writting data"<<endl;
+            return false;
+        default:
+            cout<<"Error: an unknown error has occured"<<endl;
+            return false;
+    };
+}
+
+bool DecodeStatusCodes::writeString(const char code)
+{
+    switch (code) {
+        case 1:
+            if (showSuccess)
+                cout<<"String written succesfully"<<endl;
+            return true;
+        case -1:
+            cout<<"Error: error while writting data"<<endl;
+            return false;
+        default:
+            cout<<"Error: an unknown error has occured"<<endl;
+            return false;
+    };
 }
 
 #endif // DECODESTATUSCODES_H
