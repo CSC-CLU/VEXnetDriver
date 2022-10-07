09d1e1aa0a18b034a0a3176f7e7990d8ce1d2555
sudo-Eric <ericheinke0@gmail.com>
2022-10-06 18:05:47 -0700

    Added JavaDoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"VEXnetDriver.DeviceType"},{"p":"<Unnamed>","l":"main"},{"p":"<Unnamed>","l":"VEXnetPacket.PacketType"},{"p":"<Unnamed>","l":"VEXnetDriver"},{"p":"<Unnamed>","l":"VEXnetPacket"}];updateSearchResults();
